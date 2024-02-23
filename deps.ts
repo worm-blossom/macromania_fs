9746da5d38c444fcf0748d2d0e0c260df0fc14db
Aljoscha Meyer <mail@aljoscha-meyer.de>
2024-02-23 18:46:36 +0100

    Prepare for release

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * from "../macromania/mod.ts";
 export * as fs from "https://deno.land/std@0.63.0/fs/mod.ts";
-export * from "../macromania-logger/mod.tsx";
+export * from "https://deno.land/x/macromania@v0.1.0/mod.ts";
+export * from "https://deno.land/x/logging_utils@v10.0/mod.ts";
