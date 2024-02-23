58b4ba4b51935e53bec2d4924a1b7fef3269e6d4
Aljoscha Meyer <mail@aljoscha-meyer.de>
2024-02-23 19:05:47 +0100

    Update faulty dependency

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * as fs from "https://deno.land/std@0.63.0/fs/mod.ts";
 export * from "https://deno.land/x/macromania@v0.1.0/mod.ts";
-export * from "https://deno.land/x/macromania_logger@v0.1.0/mod.tsx";
+export * from "https://deno.land/x/macromania_logger@v0.1.1/mod.tsx";
